1716aa9cd881cf0dbd93fbb00662b937ab1fe47b
DAYA_KUN <dayananth900@gmail.com>
2024-11-04 22:37:58 +0530

    Doubly Linked List

--- Linked Lists/doubly_linked_list.py
+++ Linked Lists/doubly_linked_list.py
@@ -38,12 +38,53 @@ class DoublyLinkedList:
         if new_node.next:
             new_node.next.prev=new_node
 
+    def delete_node(self,node):
+        if self.head==None or node==None:
+            return
+        if self.head==node:
+            self.head=node.next
+        if node.next:
+            node.next.prev=node.prev
+        if node.prev:
+            node.prev.next=node.next
+        node=None
+
+    def delete_node_by_value(self,value):
+        if self.head is None:
+            print(f"The Node {value} is not found in the linked list")
+            return
+
+        current=self.head
+        while current and current.data!=value:
+            current=current.next
+
+        if current is None:
+            print(f"The Node {value} is not found in the linked list")
+            return
+
+        
+
+        if current == self.head:
+            self.head = current.next
+            if self.head:  # Update the new head's prev pointer, if there is a new head
+                self.head.prev = None
+        else:
+            # Bypass the current node by adjusting pointers
+            if current.prev:
+                current.prev.next = current.next
+            if current.next:
+                current.next.prev = current.prev
+
+        # Free up memory (optional)
+        current = None
+        return
+
+
     def display(self):
         current=self.head
         while current:
             print(current.data,end=" ")
             current=current.next
-        print( )
         return
     
 dl=DoublyLinkedList()
@@ -52,6 +93,9 @@ dl.insert_at_end(2)
 dl.insert_at_end(3)
 dl.insert_at_end(4)
 dl.insert_at_end(5)
+dl.delete_node(dl.head.next.next.next)
+dl.delete_node_by_value(3)
+dl.delete_node_by_value(7)
 dl.display()
 
 
